4e4bfe07b1f67ca08a254d620f105f4f5d250dbd
Manito-ui <noguerayaneth912@gmail.com>
2026-09-10 10:41:18 -0400

    Add files via upload

---
+++ src/Qwen_cpp_20260910_amr74xb6a.cpp
@@ -0,0 +1,5 @@
+void posicion2velocidad200() { /* Intervalos y setpoints para ruta 2 */ }
+void posicion3velocidad200() { /* Intervalos y setpoints para ruta 3 */ }
+void posicion4adelante()       { /* Intervalos y setpoints para ruta 4 */ }
+void posicion5adelante()       { /* Intervalos y setpoints para ruta 5 */ }
+void posicion6adelante()       { /* Intervalos y setpoints para ruta 6 */ }
