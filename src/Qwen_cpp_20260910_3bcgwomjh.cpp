c7f115366b564fa17bdd8f74fd4ecb628535b522
Manito-ui <noguerayaneth912@gmail.com>
2026-09-10 10:39:02 -0400

    Add files via upload

---
+++ src/Qwen_cpp_20260910_3bcgwomjh.cpp
@@ -0,0 +1,20 @@
+#define Clk 3
+#define Dt 2
+volatile long ticks = 0;
+const float CM_POR_TICK = 0.01;
+
+void setup() {
+  // ...
+  attachInterrupt(digitalPinToInterrupt(Clk), ENCODER, CHANGE);
+}
+
+void ENCODER() {
+  int A = digitalRead(Clk); int B = digitalRead(Dt);
+  // Lógica de cuadratura para sumar/restar ticks
+  if (A == 1) ticks += (B == 0) ? 1 : -1; 
+}
+
+float obtenerDistanciaCm() {
+  long t; noInterrupts(); t = ticks; interrupts();
+  return t * CM_POR_TICK;
+}
